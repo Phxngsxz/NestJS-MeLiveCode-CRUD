0e6e9c47ad203f4938ae95f0da219b073b34c768
Kagenou <phongsaphon.ssi@gmail.com>
2025-03-20 10:26:28 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "",
+    "database": "nestjs-melivecoach-crud",
+    "entities": ["dist/**/*.entity{.ts,.js}"],
+    "synchronize": true
+    }),
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
